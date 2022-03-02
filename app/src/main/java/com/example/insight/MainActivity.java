a446ef66f64bb57309860f06143fa2e98bb36d24
Azoies1 <le_thom@live.concordia.ca>
2022-03-02 13:28:26 -0500

    Displaying object detection and Lidar activities.

--- app/src/main/java/com/example/insight/MainActivity.java
+++ app/src/main/java/com/example/insight/MainActivity.java
@@ -1,5 +1,19 @@
 package com.example.insight;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
+import android.os.Build;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,42 +22,204 @@ import android.view.View;
 import android.widget.Button;
 import detection.CameraActivity;
 import detection.DetectorActivity;
+import detection.customview.OverlayView;
 import lidar.LidarActivity;
+import lidar.LidarModule.LidarHelper;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button lidarButton;
-    private Button detectionButton;
+    private static final String PERMISSION_CAMERA = Manifest.permission.CAMERA;
+
+    private Intent intent;
+    private LidarActivity lidarActivity;
+    protected LidarHelper lidarHelper;
+    private DetectorActivity detectorActivity;
+
+    private Thread detectorThread;
+    private static OverlayView trackingOverlay ;
+    private static ImageView bitmapImageView;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-        Button btn = (Button)findViewById(R.id.button_maps);
-        lidarButton = findViewById(R.id.lidarButton);
-        detectionButton = findViewById(R.id.ObjDetectButton);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, GpsActivity.class));
+        //Camera Permissions
+        if(!hasPermission()){
+            requestPermission();
+        }
+
+        bitmapImageView = findViewById(R.id.bitmapImageView);
+
+        //Lidar Helper
+        try {
+            UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+            lidarHelper = new LidarHelper(manager);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        intent = getIntent();
+        if (intent != null) {
+            UsbDevice device = (UsbDevice) intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+            if (device != null) {
+                Toast.makeText(getBaseContext(), "thing: " + device, Toast.LENGTH_SHORT).show();
+                lidarHelper.connectUsb();
             }
-        });
-
-        lidarButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, LidarActivity.class));
-            }
-        });
-
-        detectionButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, DetectorActivity.class));
-            }
-        });
-
+        }
+        lidarActivity = new LidarActivity(this);
     }
 
+      @Override
+  public synchronized void onStart() {
+    super.onStart();
+  }
+
+  @Override
+  public synchronized void onResume() {
+    super.onResume();
+    if (detectorActivity == null) {
+        detectorThread = new Thread(() -> {
+            detectorActivity = new DetectorActivity(MainActivity.this, this, getSupportFragmentManager());
+        });
+        detectorThread.start();
+    }else{
+        resumeHandlerThread();
+    }
+  }
+
+  @Override
+  public synchronized void onPause() {
+      pauseHandlerThread();
+      super.onPause();
+  }
+
+//  @Override
+//  public synchronized void onStop() {
+////    LOGGER.d("onStop " + this);
+//    super.onStop();
+//  }
+//
+//  @Override
+//  public synchronized void onDestroy() {
+////    LOGGER.d("onDestroy " + this);
+//    super.onDestroy();
+//  }
+
+    @Override
+    public void onRequestPermissionsResult(
+            final int requestCode, final String[] permissions, final int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            if (!allPermissionsGranted(grantResults)) {
+                requestPermission();
+            }
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.lidar_on:
+                try {
+                    lidarHelper.sendStart3D();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return (true);
+            case R.id.lidar_off:
+                try {
+                    lidarHelper.sendStop();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return (true);
+            case R.id.lidar_bitmap:
+                //Turn off bitmap
+                bitmapImageView.setEnabled(!bitmapImageView.isEnabled());
+                return (true);
+            case R.id.objDetectSwitch:
+                if (detectorActivity.handlerThread != null) {
+                    pauseHandlerThread();
+                }
+                else{
+                    resumeHandlerThread();
+                }
+                return (true);
+        }
+        return false;
+    }
+
+    private void resumeHandlerThread(){
+        if (detectorActivity != null) {
+            HandlerThread hThread = new HandlerThread("inference");
+            detectorActivity.handlerThread = hThread;
+            hThread.start();
+            detectorActivity.handler = new Handler(hThread.getLooper());
+        }
+    }
+
+    private void pauseHandlerThread(){
+        if (detectorActivity != null) {
+            detectorActivity.handlerThread.quitSafely();
+            try {
+                detectorActivity.handlerThread.join();
+                detectorActivity.handlerThread = null;
+                detectorActivity.handler = null;
+            } catch (final InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private boolean hasPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return checkSelfPermission(PERMISSION_CAMERA) == PackageManager.PERMISSION_GRANTED;
+        } else {
+            return true;
+        }
+    }
+
+
+    private void requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (shouldShowRequestPermissionRationale(PERMISSION_CAMERA)) {
+                Toast.makeText(
+                                this,
+                                "Camera permission is required",
+                                Toast.LENGTH_LONG)
+                        .show();
+            }
+            requestPermissions(new String[] {PERMISSION_CAMERA}, 1);
+        }
+    }
+
+    private static boolean allPermissionsGranted(final int[] grantResults) {
+        for (int result : grantResults) {
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static OverlayView getTrackingOverlay(){
+        return trackingOverlay;
+    }
+
+    public static ImageView getBitmapImageView(){
+        return bitmapImageView;
+    }
 }
--- app/src/main/java/lidar/LidarActivity.java
+++ app/src/main/java/lidar/LidarActivity.java
@@ -14,6 +14,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.insight.MainActivity;
 import com.example.insight.R;
 
 import java.io.IOException;
@@ -24,7 +25,7 @@ import lidar.LidarModule.DataPoolScheduler;
 import lidar.LidarModule.LidarHelper;
 import lidar.LidarModule.LidarRenderer;
 
-public class LidarActivity extends AppCompatActivity{
+public class LidarActivity {
 
     private static final String TAG = "LidarActivity";
 
@@ -40,7 +41,13 @@ public class LidarActivity extends AppCompatActivity{
     protected Thread hapticThread;
     protected Looper looper;
     protected Handler handler;
-    @Override
+    private Context context;
+
+    public LidarActivity(Context context){
+        this.context = context;
+        this.onCreate();
+    }
+
     protected void onDestroy() {
         try {
             LidarHelper.closePort();
@@ -52,28 +59,24 @@ public class LidarActivity extends AppCompatActivity{
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
-        super.onDestroy();
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lidar);
-        try {
-            UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
-            lidarHelper = new LidarHelper(manager);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        intent = getIntent();
-        if (intent != null) {
-            UsbDevice device = (UsbDevice) intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
-            if (device != null) {
-                Toast.makeText(getBaseContext(), "thing: " + device, Toast.LENGTH_SHORT).show();
-                lidarHelper.connectUsb();
-            }
-        }
-        setupUI();
+    protected void onCreate() {
+//        try {
+//            UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+//            lidarHelper = new LidarHelper(manager);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        if (intent != null) {
+//            UsbDevice device = (UsbDevice) intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+//            if (device != null) {
+//                Toast.makeText(getBaseContext(), "thing: " + device, Toast.LENGTH_SHORT).show();
+//                lidarHelper.connectUsb();
+//            }
+//        }
+//        setupUI();
+        this.bitmapImageView = MainActivity.getBitmapImageView();
         if (dataThread == null) {
             dataThread = new Thread(new DataHandler());
             dataThread.setPriority(Thread.MAX_PRIORITY);
@@ -81,7 +84,7 @@ public class LidarActivity extends AppCompatActivity{
             dataThread.start();
         }
         if (renderThread == null) {
-            renderThread = new Thread(new LidarRenderer(this, bitmapImageView));
+            renderThread = new Thread(new LidarRenderer(this.context, bitmapImageView));
             dataThread.setName("renderThread");
             renderThread.start();
         }
@@ -97,67 +100,67 @@ public class LidarActivity extends AppCompatActivity{
         }
     }
 
-    public void setupUI() {
-
-        testButton = findViewById(R.id.testButton);
-        getInfoBbutton = findViewById(R.id.getInfoButton);
-        threeDButton = findViewById(R.id.threeDModeButton);
-        exitButton = findViewById(R.id.exitButton);
-        infoTextView = findViewById(R.id.infoTextView);
-        bitmapImageView = findViewById(R.id.bitmapImageView);
-
-        exitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    lidarHelper.sendStop();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        testButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    lidarHelper.sendSetBaud();
-                } catch (Exception e) {
-                    makeToast(e.getMessage());
-                }
-            }
-        });
-
-        getInfoBbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    if(!lidarHelper.sendInfoRequest()){
-                        Toast.makeText(getApplicationContext(),"No USB connection found.", Toast.LENGTH_SHORT).show();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                //usbGetInfo();
-            }
-        });
-
-        threeDButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    lidarHelper.sendStart3D();
-                    //threeDMode();
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
-
-    public void makeToast(String message) {
-        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
-    }
+//    public void setupUI() {
+//
+//        testButton = findViewById(R.id.testButton);
+//        getInfoBbutton = findViewById(R.id.getInfoButton);
+//        threeDButton = findViewById(R.id.threeDModeButton);
+//        exitButton = findViewById(R.id.exitButton);
+//        infoTextView = findViewById(R.id.infoTextView);
+//        bitmapImageView = findViewById(R.id.bitmapImageView);
+//
+//        exitButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                try {
+//                    lidarHelper.sendStop();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
+//
+//        testButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                try {
+//                    lidarHelper.sendSetBaud();
+//                } catch (Exception e) {
+//                    makeToast(e.getMessage());
+//                }
+//            }
+//        });
+//
+//        getInfoBbutton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                try {
+//                    if(!lidarHelper.sendInfoRequest()){
+//                        Toast.makeText(getApplicationContext(),"No USB connection found.", Toast.LENGTH_SHORT).show();
+//                    }
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//                //usbGetInfo();
+//            }
+//        });
+//
+//        threeDButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                try {
+//                    lidarHelper.sendStart3D();
+//                    //threeDMode();
+//                }catch (Exception e){
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
+//    }
+//
+//    public void makeToast(String message) {
+//        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+//    }
 
     public ImageView getBitmapImageView(){
         return bitmapImageView;
