d4cc1a0e47bd57ee16041cdf5f7ee91d823ca636
ewaywen <yiwei.wen@spaceconcordia.ca>
2022-03-03 15:19:11 -0500

    fixed some warnings

--- app/src/main/java/lidar/LidarModule/LidarRenderer.java
+++ app/src/main/java/lidar/LidarModule/LidarRenderer.java
@@ -15,22 +15,19 @@ public class LidarRenderer implements Runnable {
     private static ArrayBlockingQueue<int[]> hapticQ;
     private static int[] frameInt;
     private static int[] hapticInt;
-    private static int frameIndex;
     private final Handler handler;
-    private static ImageView bitmapImageView;
-    private static Bitmap oldBitmap;
+    private final ImageView bitmapImageView;
     private static Bitmap newBitmap;
-
 
     public LidarRenderer(Context context, ImageView bitmapImageView) {
         if (frameQ == null) LidarRenderer.frameQ = DataHandler.getFrameQ();
         if (frameInt == null) LidarRenderer.frameInt = new int[9600];
         if (hapticInt == null) LidarRenderer.hapticInt = new int[9600];
-        if (LidarRenderer.bitmapImageView == null) LidarRenderer.bitmapImageView = bitmapImageView;
         handler = new Handler(context.getMainLooper());
         LidarRenderer.bitmapQ = new ArrayBlockingQueue<>(100);
         LidarRenderer.colorQ = new ArrayBlockingQueue<>(100);
         LidarRenderer.hapticQ = new ArrayBlockingQueue<>(100);
+        this.bitmapImageView = bitmapImageView;
     }
 
     @Override
@@ -40,7 +37,7 @@ public class LidarRenderer implements Runnable {
                 //Since the data is in bytes (0x__) and each pixel is three hex values (0x___)
                 //It is necessary to split a byte such that (0x11)(0x22)(0x33) --> (0x112)(0x233)
                 byte[] frame = frameQ.take();
-                frameIndex = 0;
+                int frameIndex = 0;
                 for (int i = 1; i < 14399; i+=3) {
                     int a = frame[i]&0x0ff;
                     int b = frame[i + 1]&0x0ff;
@@ -67,7 +64,7 @@ public class LidarRenderer implements Runnable {
                 }
                 hapticQ.put(hapticInt.clone());
                 colorQ.put(frameInt.clone());
-                oldBitmap = newBitmap;
+                Bitmap oldBitmap = newBitmap;
                 newBitmap = bitmapQ.take();
                 //newBitmap = Bitmap.createBitmap(frameInt, 160, 60, Bitmap.Config.ARGB_8888);
                 runOnUiThread(new Runnable(){
